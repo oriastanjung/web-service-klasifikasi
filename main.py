0b12c6f3d901d7f8c14da4630df26b2b0aadad04
oriastanjung <oriastan999@gmail.com>
2024-06-22 12:14:46 +0700

    finish

--- main.py
+++ main.py
@@ -79,6 +79,6 @@ def upload_image():
         
         
         
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=7860,debug=True)
-    # app.run(debug=True)
+# if __name__ == '__main__':
+#     # app.run(host='0.0.0.0',port=7860,debug=True)
+#     # app.run(debug=True)
